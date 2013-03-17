b114391cc215a2e9a0a900fbc8a267bacb94bb6f
Michael L Perry <michael@qedcode.com>
2013-03-17 18:06:34 -0500

    Added WinRT model and unit test projects.

---
+++ Windows/WinRT/Correspondence.Reader.Model/Model.generated.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UpdateControls.Correspondence;
+using UpdateControls.Correspondence.Mementos;
+using UpdateControls.Correspondence.Strategy;
+using System;
+using System.IO;
+
+/**
+/ For use with http://graphviz.org/
+digraph "Correspondence.Reader.Model"
+{
+    rankdir=BT
+}
+**/
+
+namespace Correspondence.Reader.Model
+{
+    public partial class Individual : CorrespondenceFact
+    {
+		// Factory
+		internal class CorrespondenceFactFactory : ICorrespondenceFactFactory
+		{
+			private IDictionary<Type, IFieldSerializer> _fieldSerializerByType;
+
+			public CorrespondenceFactFactory(IDictionary<Type, IFieldSerializer> fieldSerializerByType)
+			{
+				_fieldSerializerByType = fieldSerializerByType;
+			}
+
+			public CorrespondenceFact CreateFact(FactMemento memento)
+			{
+				Individual newFact = new Individual(memento);
+
+				// Create a memory stream from the memento data.
+				using (MemoryStream data = new MemoryStream(memento.Data))
+				{
+					using (BinaryReader output = new BinaryReader(data))
+					{
+						newFact._anonymousId = (string)_fieldSerializerByType[typeof(string)].ReadData(output);
+					}
+				}
+
+				return newFact;
+			}
+
+			public void WriteFactData(CorrespondenceFact obj, BinaryWriter output)
+			{
+				Individual fact = (Individual)obj;
+				_fieldSerializerByType[typeof(string)].WriteData(output, fact._anonymousId);
+			}
+		}
+
+		// Type
+		internal static CorrespondenceFactType _correspondenceFactType = new CorrespondenceFactType(
+			"Correspondence.Reader.Model.Individual", 1);
+
+		protected override CorrespondenceFactType GetCorrespondenceFactType()
+		{
+			return _correspondenceFactType;
+		}
+
+        // Roles
+
+        // Queries
+
+        // Predicates
+
+        // Predecessors
+
+        // Fields
+        private string _anonymousId;
+
+        // Results
+
+        // Business constructor
+        public Individual(
+            string anonymousId
+            )
+        {
+            InitializeResults();
+            _anonymousId = anonymousId;
+        }
+
+        // Hydration constructor
+        private Individual(FactMemento memento)
+        {
+            InitializeResults();
+        }
+
+        // Result initializer
+        private void InitializeResults()
+        {
+        }
+
+        // Predecessor access
+
+        // Field access
+        public string AnonymousId
+        {
+            get { return _anonymousId; }
+        }
+
+        // Query result access
+
+        // Mutable property access
+
+    }
+    
+
+	public class CorrespondenceModel : ICorrespondenceModel
+	{
+		public void RegisterAllFactTypes(Community community, IDictionary<Type, IFieldSerializer> fieldSerializerByType)
+		{
+			community.AddType(
+				Individual._correspondenceFactType,
+				new Individual.CorrespondenceFactFactory(fieldSerializerByType),
+				new FactMetadata(new List<CorrespondenceFactType> { Individual._correspondenceFactType }));
+		}
+	}
+}
